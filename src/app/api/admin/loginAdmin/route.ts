20164be587baac69daf9ebb6d5caa4619a76bbd9
Taffyyy114 <taaffyy1234@gmail.com>
2025-04-13 03:14:52 +0800

    admin api done

---
+++ src/app/api/admin/loginAdmin/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { phoneNumber, password } = body;
+    const admin = await prisma.admins.findFirst({
+      where: { phoneNumber },
+    });
+    if (!admin) {
+      return NextResponse.json({ error: "Admin not found" });
+    }
+    const passwordValid = bcrypt.compareSync(password, admin.password);
+    if (!passwordValid) {
+      return NextResponse.json({
+        error: "Incorrect phoneNumber and password combination",
+      });
+    }
+    const token = jwt.sign({ adminId: admin.id }, process.env.JWT_SECRET!, {
+      expiresIn: "3h",
+    });
+    return NextResponse.json({ token }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Login error" }, { status: 404 });
+  }
+};

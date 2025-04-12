3c901db343e93fb28c65a01bf447a7a9a026c2ab
Taffyyy114 <taaffyy1234@gmail.com>
2025-04-12 15:25:00 +0800

    done

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -15,28 +15,26 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="bg-[#24182E] h-[100px] flex items-center justify-between p-5 drop-shadow-lg">
+    <header className="bg-[#f1b3b6] h-[100px] flex items-center justify-between p-5 ">
       <button
         onClick={() => (window.location.href = "/")}
-        className="hover:cursor-pointer pt-2"
+        className="hover:cursor-pointer bg-transparent border-none pl-4"
       >
         <img
-          className="h-[50px]"
-          src="https://live.staticflickr.com/65535/54377010981_d1792a4b59.jpg"
-          alt="Ikh Och Logo"
+          className="h-[95px] w-[130px] border-none bg-cover"
+          src="https://i.imgur.com/tfJ11tq.png"
         />
       </button>
 
-      {/* Desktop & Tablet Navigation */}
-      <div className="hidden lg:flex items-center text-[#EFEFEF]">
-        {["/", "/about", "/services", "/contact"].map((route) => (
+      <div className="hidden lg:flex items-center text-[#EFEFEF] font-bold">
+        {["/", "/about", "/products", "/contact"].map((route) => (
           <button
             key={route}
             onClick={() => (window.location.href = route)}
             className={`p-2 transition-colors duration-300 ease-out ${
               pathName === route
-                ? "text-[#FF8A00] border-b-2 border-[#FF8A00]"
-                : "hover:text-[#FF8A00] hover:border-b-2 hover:border-[#FF8A00]"
+                ? "text-[#44aeff] border-b-2 border-[#44aeff]"
+                : "hover:text-[#44aeff] hover:border-b-2 hover:border-[#44aeff]"
             }`}
           >
             {language === "en"
@@ -48,20 +46,19 @@ const Header = () => {
               ? "Нүүр"
               : route === "/about"
               ? "Бидний тухай"
-              : route === "/services"
-              ? "Үйлчилгээ"
+              : route === "/products"
+              ? "Бүтээгдэхүүн"
               : "Холбоо барих"}
           </button>
         ))}
         <button
           onClick={() => toggleLanguage(language === "en" ? "mn" : "en")}
-          className="ml-4 p-2 bg-[#FF8A00] opacity-[90%]  text-[#EFEFEF] rounded-lg hover:bg-[#FF8A00] hover:opacity-100 transition-all"
+          className="ml-4 p-2 bg-[#44aeff] opacity-[90%]  text-[#EFEFEF] rounded-lg hover:bg-[#44aeff] hover:opacity-100 transition-all"
         >
           {language === "en" ? "Монгол" : "English"}
         </button>
       </div>
 
-      {/* Mobile Navigation */}
       <div className="lg:hidden flex items-center">
         <button
           className="p-2 bg-[#FF8A00] text-[#EFEFEF] rounded-lg"
